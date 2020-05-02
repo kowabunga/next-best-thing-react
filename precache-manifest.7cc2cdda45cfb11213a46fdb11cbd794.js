9e36e0991912c9c809f2569dbd855ce2d7f6bf48
Anthony <anthony.siletti@gmail.com>
2020-05-02 01:15:09 -0400

    Updates

---
+++ precache-manifest.7cc2cdda45cfb11213a46fdb11cbd794.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48794b047469a7d4d9453025c38c9e7f",
+    "url": "/next-best-thing-react/index.html"
+  },
+  {
+    "revision": "dfb2a19f208ba478b05f",
+    "url": "/next-best-thing-react/static/css/main.bac587d2.chunk.css"
+  },
+  {
+    "revision": "7b3cf8ed86e2f35b0081",
+    "url": "/next-best-thing-react/static/js/2.048a8962.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/next-best-thing-react/static/js/2.048a8962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfb2a19f208ba478b05f",
+    "url": "/next-best-thing-react/static/js/main.d3cd473d.chunk.js"
+  },
+  {
+    "revision": "1ad96e452f008620ee89",
+    "url": "/next-best-thing-react/static/js/runtime-main.49ecb906.js"
+  },
+  {
+    "revision": "815d15ee06b98b2788a2bd9c1d21bd18",
+    "url": "/next-best-thing-react/static/media/author.815d15ee.png"
+  },
+  {
+    "revision": "5d29c044244f05f095fa50c3b9d3eeb1",
+    "url": "/next-best-thing-react/static/media/creativity-flow.5d29c044.png"
+  },
+  {
+    "revision": "a026ff2ed057688ff475e0798543e74a",
+    "url": "/next-best-thing-react/static/media/inyourgrasp.a026ff2e.png"
+  },
+  {
+    "revision": "81a319ca8ecc45e6c35795e14a18d50f",
+    "url": "/next-best-thing-react/static/media/nbtbook.81a319ca.png"
+  }
+]);
